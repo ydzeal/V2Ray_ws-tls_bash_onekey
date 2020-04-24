089cf6cad7ecb0929a432c479382a232f5986c28
ydzeal <ydzeal@users.noreply.github.com>
2020-04-25 01:44:20 +0800

    update acme path2

--- install.sh
+++ install.sh
@@ -460,27 +460,27 @@ port_exist_check() {
     fi
 }
 acme() {
-    if "$HOME"/ubuntu/.acme.sh/acme.sh --issue -d "${domain}" --standalone -k ec-256 --force --test; then
+    if /home/ubuntu/.acme.sh/acme.sh --issue -d "${domain}" --standalone -k ec-256 --force --test; then
         echo -e "${OK} ${GreenBG} SSL 证书测试签发成功，开始正式签发 ${Font}"
-        rm -rf "$HOME/.acme.sh/${domain}_ecc"
+        rm -rf "$/home/ubuntu/.acme.sh/${domain}_ecc"
         sleep 2
     else
         echo -e "${Error} ${RedBG} SSL 证书测试签发失败 ${Font}"
-        rm -rf "$HOME/.acme.sh/${domain}_ecc"
+        rm -rf "$/home/ubuntu/.acme.sh/${domain}_ecc"
         exit 1
     fi
 
-    if "$HOME"/ubuntu/.acme.sh/acme.sh --issue -d "${domain}" --standalone -k ec-256 --force; then
+    if /home/ubuntu/.acme.sh/acme.sh --issue -d "${domain}" --standalone -k ec-256 --force; then
         echo -e "${OK} ${GreenBG} SSL 证书生成成功 ${Font}"
         sleep 2
         mkdir /data
-        if "$HOME"/ubuntu/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /data/v2ray.crt --keypath /data/v2ray.key --ecc --force; then
+        if /home/ubuntu/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /data/v2ray.crt --keypath /data/v2ray.key --ecc --force; then
             echo -e "${OK} ${GreenBG} 证书配置成功 ${Font}"
             sleep 2
         fi
     else
         echo -e "${Error} ${RedBG} SSL 证书生成失败 ${Font}"
-        rm -rf "$HOME/.acme.sh/${domain}_ecc"
+        rm -rf "$/home/ubuntu/.acme.sh/${domain}_ecc"
         exit 1
     fi
 }
